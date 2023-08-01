778fb1922e5a9696d79e5c8bddba4b1e0921c9f7
Isuranga Warnasooriya <59173608+Isuranga-2001@users.noreply.github.com>
2023-08-01 23:34:32 +0530

    Complete draw wire mechanism

--- Assets/Scripts/DrawLineScript.cs
+++ Assets/Scripts/DrawLineScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
@@ -8,28 +9,60 @@ public class DrawLineScript : MonoBehaviour
     public Color LineColor;
     private LineRenderer lineRenderer;
 
+    bool isClicked = false;
+
     private void Awake()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        //lineRenderer.SetColors(LineColor, LineColor);
+        //lineRenderer.positionCount++;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        //lineRen.SetPosition(lineRen.positionCount++, new Vector3(0, 0, 0));
-        //lineRen.SetPosition(lineRen.positionCount++, new Vector3(1, 0, 0));
+        lineRenderer.positionCount++;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonUp(0) && lineRenderer.positionCount > 1)
+        {
+            Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+            
+            lineRenderer.SetPosition(lineRenderer.positionCount - 1, worldPosition);
+            lineRenderer.positionCount++;
+
+            lineRenderer.SetPosition(lineRenderer.positionCount - 1, worldPosition);
+
+            isClicked = false;
+
+        }
+        
         if (Input.GetMouseButtonDown(0))
+        {
+            isClicked = true;
+
+            if (lineRenderer.positionCount == 1)
+            {
+                Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+
+                lineRenderer.SetPosition(lineRenderer.positionCount - 1, worldPosition);
+                lineRenderer.positionCount++;
+
+                lineRenderer.SetPosition(lineRenderer.positionCount - 1, worldPosition);
+            }
+        }
+
+        if (isClicked)
         {
             Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
             Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
 
-            lineRenderer.SetPosition(lineRenderer.positionCount++, worldPosition);
+
+            lineRenderer.SetPosition(lineRenderer.positionCount - 1, worldPosition);
         }
     }
 }
