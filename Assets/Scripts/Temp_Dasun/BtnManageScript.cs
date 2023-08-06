1731d3fa09b5103c4a85e3cbd6c55deb87c46a17
dasun-madusanka <dasunmadusanka7890@gmail.com>
2023-08-06 18:03:58 +0530

    Updated btn manager

--- Assets/Scripts/Temp_Dasun/BtnManageScript.cs
+++ Assets/Scripts/Temp_Dasun/BtnManageScript.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BtnManageScript : MonoBehaviour
 {
-
+    public Button b1;
     public BreadBoardManager breadboard;
 
     public void OnUndoButtonClick()
     {
         breadboard.Undo();
+    }
+
+    public void GotoSampleScene()
+    {
+        SceneManager.LoadScene(2);
     }
 }
